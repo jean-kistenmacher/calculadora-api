78e6f22c49382cac8f6865fab3ee22d4e2fab040
Jean Kistenmacher <jean.kistenmacher@b2wdigital.com>
2023-11-19 15:05:23 -0300

    feat: add unidade in diluicaoi

--- src/controllers/DiluicaoController.ts
+++ src/controllers/DiluicaoController.ts
@@ -4,13 +4,14 @@ import { prismaClient } from '../database/prismaClient';
 export class DiluicaoController {
 
   async createDiluicao (request: Request, response: Response) {
-    const { concentracao, diluicao, estabilidade, observacao, reconstituicao, tempoAdm, idApresentacao, idAcesso, idVia } = request.body;
+    const { concentracao, diluicao, estabilidade, observacao, reconstituicao, tempoAdm, idApresentacao, idAcesso, idVia, idUnidade } = request.body;
 
     const calcDiluicao = await prismaClient.calculoDiluicao.create({
       data: {
         id_apresentacao: idApresentacao,
         id_via: idVia,
         id_acesso: idAcesso,
+        id_unidade_medida: idUnidade,
         reconstituicao: reconstituicao,
         diluicao: diluicao,
         concentracao: concentracao,
@@ -37,7 +38,8 @@ export class DiluicaoController {
         include: {
           acesso: true,
           apresentacao: true,
-          via: true
+          via: true,
+          unidadeMedida: true
         }
       }
     )
@@ -57,7 +59,7 @@ export class DiluicaoController {
 
   async updateDiluicao (request: Request, response: Response) {
     const { id } = request.params;
-    const { concentracao, diluicao, estabilidade, observacao, reconstituicao, tempoAdm, idApresentacao, idAcesso, idVia } = request.body;
+    const { concentracao, diluicao, estabilidade, observacao, reconstituicao, tempoAdm, idApresentacao, idAcesso, idVia, idUnidade } = request.body;
     const updateDiluicao = await prismaClient.calculoDiluicao.update({
       where: {
         id: parseInt(id),
@@ -66,6 +68,7 @@ export class DiluicaoController {
         id_apresentacao: idApresentacao,
         id_via: idVia,
         id_acesso: idAcesso,
+        id_unidade_medida: idUnidade,
         reconstituicao: reconstituicao,
         diluicao: diluicao,
         concentracao: concentracao,
