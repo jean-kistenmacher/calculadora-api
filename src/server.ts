40e6d2cc0f243db55c7ef0046d188991ae099099
Jean Kistenmacher <jean.kistenmacher@b2wdigital.com>
2023-11-20 18:17:49 -0300

    feat: https

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
 import express from "express";
+import { router } from "./routes";
+
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 const cors = require('cors');
 
-import { router } from "./routes";
+const privateKey = fs.readFileSync(__dirname + '/server.key');
+const certificate = fs.readFileSync(__dirname + '/server.crt');
+
+const credentials = { key: privateKey, cert: certificate };
+
 
 const app = express();
 const port = 8080;
+const portHttps = 4430;
 
 const corsOptions = {
   origin: 'http://localhost:3000',
@@ -16,4 +26,12 @@ app.use(cors());
 
 app.use(router);
 
-app.listen(port, () => console.log(`Server is Running on PORT ${port}`))
+const server = https.createServer(credentials, app);
+
+const httpServer = http.createServer((req: any, res: any) => {
+  res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
+  res.end();
+});
+
+httpServer.listen(port, () => console.log(`Server http is Running on PORT ${port}`));
+server.listen(portHttps, () => console.log(`Server https is Running on PORT ${portHttps}`));
